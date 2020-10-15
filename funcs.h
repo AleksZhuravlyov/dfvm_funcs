2d03a83ff5543bc6b5f7b332823acfb7eba707eb
Aleksandr Zhuravlyov <a.s.zhuravljov@gmail.com>
2020-10-15 09:22:43 +0500

    Template of project is created

---
+++ funcs.h
@@ -0,0 +1,10 @@
+#ifndef DFVM_FUNCS_H
+#define DFVM_FUNCS_H
+
+double calcAFunc(const double &conc, const double &poro);
+
+double calcBFunc(const double &conc,
+                 const double &diffusion, const double &poro);
+
+
+#endif //DFVM_FUNCS_H
